248d40da58f040533ddb373fe55cd8693b679bd1
chuongk <chuongk@gmail.com>
2014-06-15 15:25:34 +0800

    Add in the Project

---
+++ SteamBot/SteamBot/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SteamBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnGetPage = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(13, 13);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(434, 20);
+            this.txtURL.TabIndex = 0;
+            // 
+            // btnGetPage
+            // 
+            this.btnGetPage.Location = new System.Drawing.Point(454, 9);
+            this.btnGetPage.Name = "btnGetPage";
+            this.btnGetPage.Size = new System.Drawing.Size(81, 23);
+            this.btnGetPage.TabIndex = 1;
+            this.btnGetPage.Text = "Get Page";
+            this.btnGetPage.UseVisualStyleBackColor = true;
+            this.btnGetPage.Click += new System.EventHandler(this.btnGetPage_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(13, 40);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(522, 421);
+            this.txtResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 473);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnGetPage);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnGetPage;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ SteamBot/SteamBot/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SteamBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetPage_Click(object sender, EventArgs e)
+        {
+            string result;
+            HttpDownloader download;
+            if (txtURL.Text != "")
+            {
+                download = new HttpDownloader(txtURL.Text, null, null);
+                result = download.GetPage();
+                StreamWriter res = new StreamWriter("result.txt");
+                res.Write(result);
+                res.Close();
+                txtResult.Text = result;
+            }
+        }
+    }
+}
